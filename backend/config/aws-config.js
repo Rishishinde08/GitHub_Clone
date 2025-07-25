9eb2829eea18b35c781ac9fde2728750999df64f
Rishishinde08 <rishikeshs2003@gmail.com>
2025-07-25 10:01:00 +0530

    changes

--- backend/config/aws-config.js
+++ backend/config/aws-config.js
@@ -3,6 +3,6 @@ const AWS = require("aws-sdk");
 AWS.config.update({ region: "ap-south-1" });
 
 const s3 = new AWS.S3();
-const S3_BUCKET = "rishigit";
+const S3_BUCKET = "your_Buket_name";
 
 module.exports = { s3, S3_BUCKET };
